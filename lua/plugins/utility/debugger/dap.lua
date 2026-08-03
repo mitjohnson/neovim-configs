cc60065319cd06971858a629188b05d3b438e1f5
mitchell <mjohnson@virtual-peaker.com>
2026-08-03 10:41:52 -0400

    refactor(dap): modularize debugging setup

--- lua/plugins/utility/debugger/dap.lua
+++ lua/plugins/utility/debugger/dap.lua
@@ -3,316 +3,150 @@
 
 return {
   {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'nvim-neotest/nvim-nio' },
+    opts = {
+      controls = {
+        element = 'repl',
+        enabled = true,
+        icons = {
+          disconnect = '⏏',
+          pause      = '⏸',
+          play       = '▶',
+          run_last   = '↺',
+          step_back  = '↶',
+          step_into  = '↓',
+          step_out   = '↑',
+          step_over  = '↷',
+          terminate  = '□',
+        },
+      },
+      element_mappings = {},
+      expand_lines = true,
+      floating = {
+        border = 'rounded',
+        mappings = {
+          close = { 'q', '<Esc>' },
+        },
+      },
+      force_buffers = true,
+      icons = {
+        collapsed     = '▸',
+        current_frame = '→',
+        expanded      = '▾',
+      },
+      layouts = {
+        {
+          elements = {
+            { id = 'scopes', size = 0.25 },
+            { id = 'breakpoints', size = 0.25 },
+            { id = 'stacks', size = 0.25 },
+            { id = 'watches', size = 0.25 },
+          },
+          position = 'right',
+          size = 50,
+        },
+        {
+          elements = {
+            { id = 'repl', size = 0.5 },
+            { id = 'console', size = 0.5 },
+          },
+          position = 'bottom',
+          size = 10,
+        },
+      },
+      mappings = {
+        edit = 'e',
+        expand = { '<CR>', '<2-LeftMouse>' },
+        open = 'o',
+        remove = 'd',
+        repl = 'r',
+        toggle = 't',
+      },
+      render = {
+        indent = 1,
+        max_value_lines = 100,
+      },
+    },
+    config = function(_, opts)
+      local dapui = require('dapui')
+      local dap = require('dap')
+
+      dapui.setup(opts)
+
+      for action, operation in pairs({
+        attach = 'open',
+        launch = 'open',
+        event_terminated = 'close',
+        event_exited = 'close',
+      }) do
+        dap.listeners.before[action].dapui_config = function()
+          dapui[operation]()
+        end
+      end
+    end,
+  },
+  {
     'mfussenegger/nvim-dap',
     dependencies = {
       'rcarriga/nvim-dap-ui',
-      'nvim-neotest/nvim-nio',
       'mfussenegger/nvim-dap-python',
     },
     keys = {
-      { '<c-p>', function() require('dap').toggle_breakpoint() end, desc = 'Toggle breakpoint' },
-      { '<leader>pc', function() require('dap').clear_breakpoints() end, desc = 'Clear all breakpoints' },
-      { '<leader>pl', function() require('dap').list_breakpoints() end, desc = 'List breakpoints' },
-      { '<leader><leader>d', function()
-        if vim.fn.filereadable('.vscode/launch.json') == 1 then
-          require('dap.ext.vscode').load_launchjs()
-        end
-        require('dap').continue()
-      end, desc = 'DAP Continue' },
+      {
+        '<c-p>',
+        function()
+          require('dap').toggle_breakpoint()
+        end,
+        desc = 'Toggle breakpoint',
+      },
+      {
+        '<leader>pc',
+        function()
+          require('dap').clear_breakpoints()
+        end,
+        desc = 'Clear all breakpoints',
+      },
+      {
+        '<leader>pl',
+        function()
+          require('dap').list_breakpoints()
+        end,
+        desc = 'List breakpoints',
+      },
+      {
+        '<leader><leader>d',
+        function()
+          if vim.fn.filereadable('.vscode/launch.json') == 1 then
+            require('dap.ext.vscode').load_launchjs()
+          end
+          require('dap').continue()
+        end,
+        desc = 'DAP Continue',
+      },
     },
-    config = function()
+    import = 'plugins.utility.debugger.languages',
+    config = function(_, opts)
       local dap = require('dap')
-      local dapui = require('dapui')
-      local dap_python = require('dap-python')
 
       dap.defaults.fallback.exception_breakpoints = { 'uncaught' }
 
-      dapui.setup({
-        controls = {
-          element = 'repl',
-          enabled = true,
-          icons = {
-            disconnect = '',
-            pause = '',
-            play = '',
-            run_last = '',
-            step_back = '',
-            step_into = '',
-            step_out = '',
-            step_over = '',
-            terminate = '',
-          },
-        },
-        element_mappings = {},
-        expand_lines = true,
-        floating = {
-          border = 'rounded',
-          mappings = {
-            close = { 'q', '<Esc>' },
-          },
-        },
-        force_buffers = true,
-        icons = {
-          collapsed = '',
-          current_frame = '',
-          expanded = '',
-        },
-        layouts = {
-          {
-            elements = {
-              {
-                id = 'scopes',
-                size = 0.25,
-              },
-              {
-                id = 'breakpoints',
-                size = 0.25,
-              },
-              {
-                id = 'stacks',
-                size = 0.25,
-              },
-              {
-                id = 'watches',
-                size = 0.25,
-              },
-            },
-            position = 'right',
-            size = 50,
-          },
-          {
-            elements = {
-              {
-                id = 'repl',
-                size = 0.5,
-              },
-              {
-                id = 'console',
-                size = 0.5,
-              },
-            },
-            position = 'bottom',
-            size = 10,
-          },
-        },
-        mappings = {
-          edit = 'e',
-          expand = { '<CR>', '<2-LeftMouse>' },
-          open = 'o',
-          remove = 'd',
-          repl = 'r',
-          toggle = 't',
-        },
-        render = {
-          indent = 1,
-          max_value_lines = 100,
-        },
-      })
+      require('dap-python').setup('python3')
 
-      dap_python.setup('python3')
+      for name, adapter in pairs(opts.adapters or {}) do
+        dap.adapters[name] = adapter
+      end
 
-      for _, adapterType in ipairs({ 'node', 'chrome', 'msedge' }) do
-        local pwaType = 'pwa-' .. adapterType
-
-        dap.adapters[pwaType] = {
-          type = 'server',
-          host = 'localhost',
-          port = '${port}',
-          executable = {
-            command = 'node',
-            args = {
-              vim.fn.stdpath('data')
-                .. '/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js',
-              '${port}',
-            },
-          },
-        }
-
-        -- this allow us to handle launch.json configurations
-        -- which specify type as "node" or "chrome" or "msedge"
-        dap.adapters[adapterType] = function(cb, config)
-          local nativeAdapter = dap.adapters[pwaType]
-
-          config.type = pwaType
-
-          if type(nativeAdapter) == 'function' then
-            nativeAdapter(cb, config)
-          else
-            cb(nativeAdapter)
+      for ft, configs in pairs(opts.configurations or {}) do
+        dap.configurations[ft] = configs
+        dap.listeners.on_config[ft] = function(config)
+          local c = vim.deepcopy(config)
+          if type(c.args) == 'string' then
+            c.args = require('dap.utils').splitstr(c.args)
           end
+          return c
         end
       end
-
-      local enter_launch_url = function()
-        local co = coroutine.running()
-        return coroutine.create(function()
-          vim.ui.input(
-            { prompt = 'Enter URL: ', default = 'http://localhost:' },
-            function(url)
-              if url == nil or url == '' then
-                return
-              else
-                coroutine.resume(co, url)
-              end
-            end
-          )
-        end)
-      end
-
-      for _, language in ipairs({
-        'typescript',
-        'javascript',
-        'typescriptreact',
-        'javascriptreact',
-        'vue',
-      }) do
-        dap.configurations[language] = {
-          {
-            type = 'pwa-node',
-            request = 'launch',
-            name = 'Launch file using Node.js (nvim-dap)',
-            program = '${file}',
-            cwd = '${workspaceFolder}',
-          },
-          {
-            type = 'pwa-node',
-            request = 'attach',
-            name = 'Attach to process using Node.js (nvim-dap)',
-            processId = require('dap.utils').pick_process,
-            cwd = '${workspaceFolder}',
-          },
-          -- requires ts-node to be installed globally or locally
-          {
-            type = 'pwa-node',
-            request = 'launch',
-            name = 'Launch file using Node.js with ts-node/register (nvim-dap)',
-            program = '${file}',
-            cwd = '${workspaceFolder}',
-            runtimeArgs = { '-r', 'ts-node/register' },
-          },
-          {
-            type = 'pwa-chrome',
-            request = 'launch',
-            name = 'Launch Chrome (nvim-dap)',
-            url = enter_launch_url,
-            webRoot = '${workspaceFolder}',
-            sourceMaps = true,
-          },
-          {
-            type = 'pwa-msedge',
-            request = 'launch',
-            name = 'Launch Edge (nvim-dap)',
-            url = enter_launch_url,
-            webRoot = '${workspaceFolder}',
-            sourceMaps = true,
-          },
-        }
-      end
-
-      dap.adapters.coreclr = {
-        type = 'executable',
-        command = 'C:/Users/sfree/AppData/Local/nvim-data/mason/packages/netcoredbg/netcoredbg/netcoredbg.exe',
-        args = { '--interpreter=vscode' },
-      }
-
-      local dotnet_build_project = function()
-        local default_path = vim.fn.getcwd() .. '/'
-
-        if vim.g['dotnet_last_proj_path'] ~= nil then
-          default_path = vim.g['dotnet_last_proj_path']
-        end
-
-        local path =
-          vim.fn.input('Path to your *proj file', default_path, 'file')
-
-        vim.g['dotnet_last_proj_path'] = path
-
-        local cmd = 'dotnet build -c Debug ' .. path .. ' > /dev/null'
-
-        print('')
-        print('Cmd to execute: ' .. cmd)
-
-        local f = os.execute(cmd)
-
-        if f == 0 then
-          print('\nBuild: ✔️ ')
-        else
-          print('\nBuild: ❌ (code: ' .. f .. ')')
-        end
-      end
-
-      local dotnet_get_dll_path = function()
-        local request = function()
-          return vim.fn.input(
-            'Path to dll to debug: ',
-            vim.fn.getcwd() .. '/bin/Debug/',
-            'file'
-          )
-        end
-
-        if vim.g['dotnet_last_dll_path'] == nil then
-          vim.g['dotnet_last_dll_path'] = request()
-        else
-          if
-            vim.fn.confirm(
-              'Change the path to dll?\n' .. vim.g['dotnet_last_dll_path'],
-              '&yes\n&no',
-              2
-            ) == 1
-          then
-            vim.g['dotnet_last_dll_path'] = request()
-          end
-        end
-
-        return vim.g['dotnet_last_dll_path']
-      end
-
-      dap.configurations.cs = {
-        {
-          type = 'coreclr',
-          name = 'Launch - coreclr (nvim-dap)',
-          request = 'launch',
-          program = function()
-            if vim.fn.confirm('Rebuild first?', '&yes\n&no', 2) == 1 then
-              dotnet_build_project()
-            end
-
-            return dotnet_get_dll_path()
-          end,
-        },
-      }
-
-      local convertArgStringToArray = function(config)
-        local c = {}
-
-        for k, v in pairs(vim.deepcopy(config)) do
-          if k == 'args' and type(v) == 'string' then
-            c[k] = require('dap.utils').splitstr(v)
-          else
-            c[k] = v
-          end
-        end
-
-        return c
-      end
-
-      for key, _ in pairs(dap.configurations) do
-        dap.listeners.on_config[key] = convertArgStringToArray
-      end
-
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-
     end,
   },
 }
---
+++ lua/plugins/utility/debugger/languages/javascript.lua
@@ -0,0 +1,103 @@
+local enter_launch_url = function()
+  local co = coroutine.running()
+  return coroutine.create(function()
+    vim.ui.input(
+      { prompt = 'Enter URL: ', default = 'http://localhost:' },
+      function(url)
+        if url == nil or url == '' then
+          return
+        else
+          coroutine.resume(co, url)
+        end
+      end
+    )
+  end)
+end
+
+local pwa_alias = function(pwa_type)
+  return function(cb, config)
+    config.type = pwa_type
+    local resolved = require('dap').adapters[pwa_type]
+    if type(resolved) == 'function' then
+      resolved(cb, config)
+    else
+      cb(resolved)
+    end
+  end
+end
+
+local pwa_adapter = {
+  type = 'server',
+  host = 'localhost',
+  port = '${port}',
+  executable = {
+    command = 'node',
+    args = {
+      vim.fn.stdpath('data')
+        .. '/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js',
+      '${port}',
+    },
+  },
+}
+
+local js_configs = {
+  {
+    type = 'pwa-node',
+    request = 'launch',
+    name = 'Launch file using Node.js (nvim-dap)',
+    program = '${file}',
+    cwd = '${workspaceFolder}',
+  },
+  {
+    type = 'pwa-node',
+    request = 'attach',
+    name = 'Attach to process using Node.js (nvim-dap)',
+    processId = require('dap.utils').pick_process,
+    cwd = '${workspaceFolder}',
+  },
+  {
+    type = 'pwa-node',
+    request = 'launch',
+    name = 'Launch file using Node.js with ts-node/register (nvim-dap)',
+    program = '${file}',
+    cwd = '${workspaceFolder}',
+    runtimeArgs = { '-r', 'ts-node/register' },
+  },
+  {
+    type = 'pwa-chrome',
+    request = 'launch',
+    name = 'Launch Chrome (nvim-dap)',
+    url = enter_launch_url,
+    webRoot = '${workspaceFolder}',
+    sourceMaps = true,
+  },
+  {
+    type = 'pwa-msedge',
+    request = 'launch',
+    name = 'Launch Edge (nvim-dap)',
+    url = enter_launch_url,
+    webRoot = '${workspaceFolder}',
+    sourceMaps = true,
+  },
+}
+
+return {
+  'mfussenegger/nvim-dap',
+  opts = {
+    adapters = {
+      ['pwa-node']   = pwa_adapter,
+      ['pwa-chrome'] = pwa_adapter,
+      ['pwa-msedge'] = pwa_adapter,
+      node   = pwa_alias('pwa-node'),
+      chrome = pwa_alias('pwa-chrome'),
+      msedge = pwa_alias('pwa-msedge'),
+    },
+    configurations = {
+      typescript = js_configs,
+      javascript = js_configs,
+      typescriptreact = js_configs,
+      javascriptreact = js_configs,
+      vue = js_configs,
+    },
+  },
+}
